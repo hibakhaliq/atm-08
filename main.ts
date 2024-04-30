a021d187da43a54ffbd09a037a812cf321df6dee
hibakhaliq <167900653+hibakhaliq@users.noreply.github.com>
2024-04-30 23:37:42 +0500

    created an atm

---
+++ main.ts
@@ -0,0 +1,41 @@
+import inquirer from "inquirer" ;
+
+let myBalance = 10000;// Dollars
+let myPin = 1234;
+
+let pinAnswer = await inquirer.prompt ([{
+
+    name: "pin",
+    message: "enter your pin",
+    type: "number"
+
+}]);
+if (pinAnswer.pin === myPin ) {
+    console.log ("Correct pincode!");
+
+    let oprationsAns = await inquirer.prompt([{
+        name: "opration",
+        message: "please select an option",
+        type: "list",
+        choices: ["withdraw money", "check balance"]
+
+    }]);
+    console.log(oprationsAns);
+    if (oprationsAns.opration === "withdraw money") {
+        let amountAns = await inquirer.prompt([
+            {
+                name: "amount",
+                message: "enter your amount",
+                type: "number"
+            }
+
+
+        ]);
+        myBalance -= amountAns.amount;
+        console.log ("Your remaining balance is:" + myBalance)
+    }
+}
+    else {
+        console.log ("Incorrect pincode")
+    }
+
